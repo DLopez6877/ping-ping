ee640ae4399f0fac184532113b828b6cbdf17487
Daniel <dlopez6877@gmail.com>
2017-05-05 09:41:30 -0700

    setup initial javascript function to test user input. Add clean log function to only display last 5 numbers

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Business Logic (Back-end)
+// var leapYear = function(year) {
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+var pingPong = function(userInput) {
+  return userInput;
+}
+
+
+// User Interface Logic (Front-end) below this point
+$(document).ready(function() {
+
+  var cleanLog = function() {
+    $("#log li:nth-child(6)").remove();
+  };
+
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#numberInput").val());
+    var result = pingPong(userInput);
+    $("#log").prepend("<li>" + result + "</li>");
+    cleanLog();
+  });
+
+});
